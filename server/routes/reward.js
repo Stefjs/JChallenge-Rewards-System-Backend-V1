ef26e986086f386f18e62287df43117d851d74bb
stefjs <r0655338@student.thomasmore.be>
2018-11-21 23:14:24 +0100

    refactoring task & reward

--- server/routes/reward.js
+++ server/routes/reward.js
@@ -11,89 +11,13 @@ var {
     User
 } = require('../models/user');
 
-app.get('/v1/rewards', (req, res) => {
-    Reward.find({accepted: false}).then((rewards) => {
-
-        if(rewards.length === 0) {
-            return res.status(400).send({
-              message: 'Geen rewards gevonden die moeten goedgekeurd worden'
-            });
-        }
-
-        User.findOne({
-            rewards: rewards[0]._id
-          }).then((user) => {
-
-            if(!user) {
-                return res.status(400).send({
-                  message: 'Geen rewards gevonden die moeten goedgekeurd worden'
-                });
-            }
-
-           return res.status(200).send({
-            name: user.name,
-            rewards
-           });
-          });  
-    }, (e) => {
-        return res.status(400).send({
-            message: 'Er is iets mis gegaan'
-        });
-    });
-});
-
-// app.post('/v1/reward/add', (req, res) => {
-//     var reward = new Reward({
-//         title: req.body.title,
-//         points: req.body.points,
-//         description: req.body.description
-//     });
-
-//     if (!req.body.token) {
-//         return res.status(400).send({
-//             message: 'Foute login'
-//         });
-//     }
-
-//     User.findOne({
-//         token: req.body.token
-//     }).then((user) => {
-
-//         if (user.type === 'admin') {
-//             reward.save().then((doc) => {
-//                 return res.status(200).send(doc);
-//             }, (e) => {
-//                 return res.status(400).send({
-//                     message: 'Er is iets mis gegaan'
-//                 });
-//             });
-//         } else {
-//             return res.status(400).send({
-//                 message: 'Foute login'
-//             });
-//         }
-//     });
-// });
-
 app.get('/v1/rewards/feed/:limit', (req, res) => {
     var limit = req.params.limit;
-
-    if (!parseInt(limit)) {
-        limit = 3;
-    }
-
-    Reward.find({
-            accepted: true
-        }).sort({
-            _id: '-1'
-        }).limit(parseInt(limit))
-        .then((rewards) => {
-            return res.status(200).send(
-                rewards
-            );
-        }, (e) => {
-            return res.status(400).send({
-                message: 'Er is iets mis gegaan'
-            });
-        });
-});
+  
+    if (!parseInt(limit)) {limit = 3;}
+    Reward.find({accepted: true}).sort({_id: '-1'}).limit(parseInt(limit))
+      .then((rewards) => {
+        if (!rewards || rewards.length === 0) {return res.status(400).send({message: 'Geen rewards gevonden'});}
+        return res.status(400).send(rewards);
+      });
+  });
--- server/routes/task.js
+++ server/routes/task.js
@@ -9,93 +9,13 @@ var {
   User
 } = require('../models/user');
 
-// app.post('/v1/task/add', (req, res) => {
-//   var task = new Task({
-//     title: req.body.title,
-//     points: req.body.points,
-//     description: req.body.description
-//   });
-
-//   if (!req.body.token) {
-//     return res.status(400).send({
-//       message: 'Foute login'
-//     });
-//   }
-
-//   User.findOne({
-//     token: req.body.token
-//   }).then((user) => {
-
-//     if (user.type === 'admin') {
-//       task.save().then((doc) => {
-//         return res.status(200).send(doc);
-//       }, (e) => {
-//         res.status(400).send({
-//           message: 'Er is iets mis gegaan'
-//         });
-//       });
-//     } else {
-//       res.status(400).send({
-//         message: 'Foute login'
-//       });
-//     }
-//   });
-// });
-
-app.get('/v1/tasks', (req, res) => {
-  Task.find({accepted: false}).then((tasks) => {
-
-    if(tasks.length === 0) {
-      return res.status(400).send({
-        message: 'Geen tasks gevonden die moeten goedgekeurd worden'
-      });
-    }
-
-    console.log(tasks[0]._id);
-
-    User.findOne({
-      rewards: tasks[0]._id
-    }).then((user) => {
-      console.log(user);
-      if(!user) {
-        return res.status(400).send({
-          message: 'Geen tasks gevonden die moeten goedgekeurd worden'
-        });
-      }
-
-     return res.status(200).send({
-      name: user.name,
-      tasks
-     });
-    });  
-  }, (e) => {
-    return res.status(400).send({
-      message: 'Er is iets mis gegaan'
-    });
-  });
-
-  
-});
-
 app.get('/v1/tasks/feed/:limit', (req, res) => {
   var limit = req.params.limit;
 
-  if (!parseInt(limit)) {
-    limit = 3;
-  }
-
-  Task.find({
-      accepted: true
-    }).sort({
-      _id: '-1'
-    }).limit(parseInt(limit))
+  if (!parseInt(limit)) {limit = 3;}
+  Task.find({accepted: true}).sort({_id: '-1'}).limit(parseInt(limit))
     .then((tasks) => {
-      return res.status(200).send(
-        tasks
-      );
-    }, (e) => {
-      res.status(400).send({
-        message: 'Er is iets mis gegaan'
-      });
+      if (!tasks || tasks.length === 0) {return res.status(400).send({message: 'Geen tasks gevonden'});}
+      return res.status(400).send(tasks);
     });
 });
